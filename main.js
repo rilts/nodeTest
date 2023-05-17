929b93a10e6681f62e5b29bbc2d2fedadf32a3e6
rilts <rialtoksj@yahoo.com>
2023-05-17 17:43:26 +0900

    JS시작하기 데이터타입 확인

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-import _ from 'lodash'
+import getType from './getType'
 
-console.log("hello world");
-console.log(_.camelCase('hello world'));
+//console.log("hello world");
+//console.log(_.camelCase('hello world'));
+
+console.log(getType(123));
+console.log(getType(false));
+console.log(getType(undefined));
+console.log(getType(null));
+console.log(getType({}));
+console.log(getType([]));
