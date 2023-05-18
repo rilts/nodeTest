02d06620a02db816a469ecaebbcdc2bb2709999e
rilts <rialtoksj@yahoo.com>
2023-05-18 18:04:09 +0900

    arrow, function's this

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ console.log(getType(undefined));
 console.log(getType(null));
 console.log(getType({}));
 console.log(getType([]));
+
+const cat = {
+  name: "meow",
+  foo1: function() {
+    console.log("foo1");
+    const foo2 = function() {
+      console.log(this.name);
+    }
+    foo2();
+  }
+}
